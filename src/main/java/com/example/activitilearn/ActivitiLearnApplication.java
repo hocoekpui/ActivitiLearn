99098ca44cea7428b46c8f5b4da1ad3d708adc27
hocoekpui <744193378@qq.com>
2023-08-13 16:39:06 +0800

    项目初始化

---
+++ src/main/java/com/example/activitilearn/ActivitiLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.activitilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiLearnApplication.class, args);
+	}
+
+}
